b973465ed06f234100e6ab049f357daf161a2304
zhmaksim <107649404+zhmaksim@users.noreply.github.com>
2024-09-30 13:54:55 +0300

    Инициализировать GPIO
    Инициализировать GPIO для OCTOSPI и LED

--- Boot/Application/core/main.c
+++ Boot/Application/core/main.c
@@ -22,6 +22,7 @@
 #include "pwr.h"
 #include "flash.h"
 #include "rcc.h"
+#include "gpio.h"
 
 /* Private macros ---------------------------------------------------------- */
 
@@ -74,6 +75,7 @@ static void setup_hardware(void)
     flash_init();
     rcc_init();
     systick_init(RCC_CPU_CLOCK);
+    gpio_init();
 }
 /* ------------------------------------------------------------------------- */
 
